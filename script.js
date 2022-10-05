e8c8fce663b1437fb5c324835c82654153477139
T <87828717+feitieng@users.noreply.github.com>
2022-10-04 22:53:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var questionsSection = document.querySelector("#questions")
+var timerSection = document.querySelector("#timer")
+var optionsSection = document.querySelector("#options")
+var submitScoreBtn = document.querySelector("#submit-score")
+var startBtn = document.querySelector("#start-button")
+var nameSection = document.querySelector("#name")
+var restartBtn = document.querySelector("#restart")
+
